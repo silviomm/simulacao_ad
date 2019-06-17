510e88164d2cb17021f0acec5689442e7e09390c
Silvio <silviommattos@gmail.com>
2019-06-17 22:29:06 +0000

    init simulacao MM1

--- bundle.js
+++ bundle.js
@@ -959,8 +959,48 @@ if ((typeof module) == 'object' && module.exports) {
 },{"crypto":1}],10:[function(require,module,exports){
 const utils = require('./utils');
 
-for (let i = 0; i < 100; i++) {
-    console.log(utils.getRandomExp(0.5));
+// taxa_chegada
+const lamb = 0.5;
+// taxa_atendimento
+const mu = 0.2;
+// # rodadas
+const QNTD_RODADAS = 100;
+// tempo atual de simulação
+let tempoSimulacao = 0;
+// array de chegadas
+let pessoas = [];
+// tempo maximo de simulacao
+const tempoMax = 100;
+
+    tempoChegada = utils.getRandomExp(lamb);
+    tempoAtendimento = utils.getRandomExp(mu);
+    tempoSimulacao = tempoChegada;
+    
+    // chegada primeira pessoa
+    pessoas.push({'chegada': tempoChegada, 'saida': tempoChegada+tempoAtendimento});
+
+while(true) {
+    tempoChegada = utils.getRandomExp(lamb);
+    tempoAtendimento = utils.getRandomExp(mu);
+
+    momentoChegada = tempoChegada + tempoSimulacao;
+    if(momentoChegada > tempoMax)
+        break;
+    
+    let momentoSaida;
+    let ultimoASair = pessoas[pessoas.length-1].saida;
+    if(ultimoASair > momentoChegada)
+        momentoSaida = ultimoASair + tempoAtendimento
+    else
+        momentoSaida = momentoChegada + tempoAtendimento
+    
+    pessoas.push({'chegada': momentoChegada, 'saida': momentoSaida});
+    tempoSimulacao += tempoChegada;
+}
+
+for (let i = 0; i < pessoas.length; i++) {
+    const element = pessoas[i];
+    document.getElementById('oie').innerHTML += `Pessoa ${i} <br> Chegada: ${element.chegada} <br> Saida: ${element.saida} <br><br>` 
 }
 },{"./utils":11}],11:[function(require,module,exports){
 const seedrandom = require('seedrandom');
