894ef5fabdc7362b1af7bde9a8daa760efb53de4
Silvio <silviommattos@gmail.com>
2019-06-17 21:33:45 +0000

    first blood

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const utils = require('./utils');
+
+for (let i = 0; i < 100; i++) {
+    console.log(utils.getRandomExp(0.5));
+}
---
+++ scripts/utils.js
@@ -0,0 +1,8 @@
+const seedrandom = require('seedrandom');
+const randomSeed = seedrandom();
+
+module.exports = {
+    getRandomExp: (rate) => {
+        return - Math.log(1 - randomSeed()) / rate;
+    }
+}
