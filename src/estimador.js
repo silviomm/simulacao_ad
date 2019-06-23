7005f771b1bd9cd9f2f591ca677bbe9c8abbc218
Leonardo Dagnino <leodag.sch@gmail.com>
2019-06-23 11:02:21 -0300

    criada classe para estimador

---
+++ src/estimador.js
@@ -0,0 +1,32 @@
+class Estimador {
+    constructor() {
+        this.estimadorMedia = 0
+        this.estimadorVariancia = 0
+        this.n = 0
+    }
+
+    acontece(n) {
+        this.estimadorMedia += n
+        this.estimadorVariancia += n ** 2
+        this.n += 1
+    }
+
+    calculaMedia() {
+        if (this.n < 1) {
+            return Infinity
+        }
+        return this.estimadorMedia/this.n
+    }
+
+    calculaVariancia() {
+        if (this.n < 2) {
+            return Infinity
+        }
+        return (this.estimadorVariancia / (this.n - 1))
+            - (this.estimadorMedia ** 2)/(this.n*(this.n-1))
+    }
+
+    calculaDesvioPadrao() {
+        return Math.sqrt(this.calculaVariancia())
+    }
+}
