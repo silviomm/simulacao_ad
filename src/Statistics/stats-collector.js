8f3d1570735169cb178ce2b10d8ed079b086c6d0
Silvio <silviommattos@gmail.com>
2019-06-25 18:26:28 +0000

    refactor statistics

---
+++ src/Statistics/stats-collector.js
@@ -0,0 +1,68 @@
+const DiscreteEstimator = require('./discrete-estimator');
+const ContinuousEstimator = require('./continuous-estimator');
+
+class StatsCollector {
+    constructor() {
+        this.round = 0;
+        this.perRound = [];
+        this.resetRoundEstimators(0);
+
+        this.rNq = new ContinuousEstimator();
+        this.X = new DiscreteEstimator();
+        this.W = new DiscreteEstimator();
+        this.T = new DiscreteEstimator();
+        this.Nq = new DiscreteEstimator();
+    }
+
+    resetRoundEstimators(time) {
+        // salvando as metricas de cada rodada antes de resetar(menos no tempo 0 que é a inicialização)
+        if (time != 0)
+            this.perRound.push({
+                'round': this.round,
+                'X': {
+                    'avg': this.rX.getAverage().toFixed(5),
+                    'var': this.rX.getVariance().toFixed(5)
+                },
+                'W': {
+                    'avg': this.rW.getAverage().toFixed(5),
+                    'var': this.rW.getVariance().toFixed(5)
+                },
+                'T': {
+                    'avg': this.rT.getAverage().toFixed(5),
+                    'var': this.rT.getVariance().toFixed(5)
+                },
+                'Nq': {
+                    'avg': this.rNq.getAverage(time).toFixed(5),
+                    'var': this.rNq.getVariance(time).toFixed(5)
+                }
+            });
+
+        this.rX = new DiscreteEstimator();
+        this.rW = new DiscreteEstimator();
+        this.rT = new DiscreteEstimator();
+        // this.rNq = new ContinuousEstimator(time);
+    }
+
+    fromElement(elt) {
+        this.rX.sample(elt.exitTime - elt.entryTime);
+        this.rW.sample(elt.entryTime - elt.arrivalTime);
+        this.rT.sample(elt.exitTime - elt.arrivalTime);
+    }
+
+    updateQueue(time, nq) {
+        this.rNq.sample(time, nq);
+    }
+
+    nextRound(time) {
+        this.round += 1;
+
+        this.X.sample(this.rX.getAverage());
+        this.W.sample(this.rW.getAverage());
+        this.T.sample(this.rT.getAverage());
+        this.Nq.sample(this.rNq.getAverage(time));
+
+        this.resetRoundEstimators(time);
+    }
+}
+
+module.exports = StatsCollector;
--- src/simulador.js
+++ src/simulador.js
@@ -1,9 +1,9 @@
 const utils = require('./utils');
-const DiscreteEstimator = require('./discrete-estimator');
-const ContinuousEstimator = require('./continuous-estimator');
 
 const LCFSQueue = require('./Queues/lcfs');
 const FCFSQueue = require('./Queues/fcfs');
+
+const StatsCollector = require('./Statistics/stats-collector');
 
 class ArrivalGenerator {
     constructor(rate, time = 0) {
@@ -57,70 +57,6 @@ class ExponentialServer {
     }
 }
 
-class Stats {
-    constructor() {
-        this.round = 0;
-        this.perRound = [];
-        this.resetRoundEstimators(0);
-
-        this.rNq = new ContinuousEstimator();
-        this.X = new DiscreteEstimator();
-        this.W = new DiscreteEstimator();
-        this.T = new DiscreteEstimator();
-        this.Nq = new DiscreteEstimator();
-    }
-
-    resetRoundEstimators(time) {
-        // salvando as metricas de cada rodada antes de resetar(menos no tempo 0 que é a inicialização)
-        if (time != 0)
-            this.perRound.push({
-                'round': this.round,
-                'X': {
-                    'avg': this.rX.getAverage().toFixed(5),
-                    'var': this.rX.getVariance().toFixed(5)
-                },
-                'W': {
-                    'avg': this.rW.getAverage().toFixed(5),
-                    'var': this.rW.getVariance().toFixed(5)
-                },
-                'T': {
-                    'avg': this.rT.getAverage().toFixed(5),
-                    'var': this.rT.getVariance().toFixed(5)
-                },
-                'Nq': {
-                    'avg': this.rNq.getAverage(time).toFixed(5),
-                    'var': this.rNq.getVariance(time).toFixed(5)
-                }
-            });
-
-        this.rX = new DiscreteEstimator();
-        this.rW = new DiscreteEstimator();
-        this.rT = new DiscreteEstimator();
-        // this.rNq = new ContinuousEstimator(time);
-    }
-
-    fromElement(elt) {
-        this.rX.sample(elt.exitTime - elt.entryTime);
-        this.rW.sample(elt.entryTime - elt.arrivalTime);
-        this.rT.sample(elt.exitTime - elt.arrivalTime);
-    }
-
-    updateQueue(time, nq) {
-        this.rNq.sample(time, nq);
-    }
-
-    nextRound(time) {
-        this.round += 1;
-
-        this.X.sample(this.rX.getAverage());
-        this.W.sample(this.rW.getAverage());
-        this.T.sample(this.rT.getAverage());
-        this.Nq.sample(this.rNq.getAverage(time));
-
-        this.resetRoundEstimators(time);
-    }
-}
-
 function queueToServer(time, queue, server) {
     let elt = queue.get();
     server.enter(time, elt);
@@ -164,7 +100,7 @@ module.exports = {
         let generator = new ArrivalGenerator(inputs.rho);
         let queue = inputs.disciplina === 'FCFS' ? new FCFSQueue() : new LCFSQueue();
         let server = new ExponentialServer(1);
-        let stats = new Stats();
+        let stats = new StatsCollector();
 
         let currentTime = 0;
 
