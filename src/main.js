2efcae56c904006f80361072d6a7667b563b2f98
Silvio <silviommattos@gmail.com>
2019-06-25 18:52:40 +0000

    refactor gerarGraficos() para dentro de Interface

--- src/main.js
+++ src/main.js
@@ -13,10 +13,7 @@ const simulator = require('./Simulator/simulator');
 
 // Adiciona evento de 'click' no botão de play.
 document.getElementById('run-button').addEventListener('click', () => {
-    executa();
-});
-
-function executa() {
+    
     let result = simulator.run(interface.getInputValues());
 
     // tabelas
@@ -24,76 +21,6 @@ function executa() {
     interface.fillMetricasTable(result.stats);
 
     // graficos
-    trataDados(result.totalId, result.nqIter, result.numPontos);
+    interface.geraGrafico(result.totalId, result.nqIter, result.numPontos, '#chartNq1');
 
-}
-
-function trataDados(totalId, nqIter, numPontos){
-
-    
-
-    // nq = rodadas.map(r => r.metricas['Nq_'])
-
-    // nq = rodadas.map(r => r.metricas['NqIter'])
-
-    console.log(totalId);
-    console.log(nqIter);
-    console.log(numPontos);
-
-    geraGrafico(totalId,nqIter, numPontos, '#chartNq1')
-
-//     nq = rodadas.map(r => r.metricas['Nq_'])
-//     console.log(nq)
-//     var data = {
-    
-//         // labels: 
-//         // ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-
-//         series: 
-//         [nq]
-//     };
-
-// // Create a new line chart object where as first parameter we pass in a selector
-// // that is resolving to our chart container element. The Second parameter
-// // is the actual data object.
-// new Chartist.Line('.ct-chart', data);
-}
-
-function geraGrafico(nTotal, dataPerTime, nPoints, chartId){
-
-    let labelArray = [];
-    //let interval = parseInt(nTotal / nPoints, 10); // De quantos em quantos clientes os dados foram coletados
-    let interval = nTotal / nPoints;
-
-    for (let i = 0; i < nPoints; i++) {
-        labelArray[i] = i*interval;
-    }
-
-    let labelQuantity = parseInt(nPoints / 10, 10);
-
-
-    const dataRhoChart = {
-            labels: labelArray,
-            series: [dataPerTime], 
-    };
-
-    const optionsRhoChart = {
-        
-        axisX: {
-            labelInterpolationFnc: function skipLabels(value, index) {
-                return ((index) % labelQuantity) === 0 ? (value) : null;
-            }
-        },
-        axisY: {
-            low: 0,
-        },
-        lineSmooth: Chartist.Interpolation.cardinal({
-            tension: 0
-        }),
-        height: 200,
-        chartPadding: { top: 30, right: 5, bottom: 0, left: 0},
-        showPoint: false,
-    };
-
-    let rhoChart = new Chartist.Line(chartId, dataRhoChart, optionsRhoChart);
-}
+});
